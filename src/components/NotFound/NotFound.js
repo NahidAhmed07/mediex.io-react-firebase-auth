32041b8db05be84250a0f02a534e78d127588676
NahidAhmed07 <mdnahidahmed2002@gmail.com>
2021-10-17 22:40:05 +0600

    first commit

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>page not fount</h1>
+    </div>
+  );
+};
+
+export default NotFound;<h1>page not fount</h1>
---
+++ src/components/Shared/Menubar/Menubar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Menubar = () => {
+  return (
+    <div>
+      <h3>nav</h3>
+
+    </div>
+  );
+};
+
+export default Menubar;
